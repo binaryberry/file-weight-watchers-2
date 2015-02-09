83c72d26491d8411f604a559b6404687505d4ff3
Tatiana Soukiassian <binaryberry@gmail.com>
2015-02-09 11:05:33 +0000

    user login unit test now passing

--- app/lib/user.rb
+++ app/lib/user.rb
@@ -1,11 +1,17 @@
 class User
 
-	attr_accessor :email, :password
+	attr_accessor :email, :password, :my_cookie
 
+	WORKSHARE_USER_SESSION_URL = 'https://my.workshare.com/api/v1.1/user_sessions.json'
 
 	def initialize(email, password)
 		@email = email
 		@password = password
+	end
+
+	def login
+		response = RestClient.post WORKSHARE_USER_SESSION_URL, {user_session:{email: @email, password: @password}, device:{app_uid: ENV['WORKSHARE_API_KEY']}}
+		@my_cookie = response.cookies
 	end
 
 
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,12 +1,22 @@
 require 'spec_helper'
 require './app/lib/user.rb'
+require 'pry'
 
 describe 'user' do
 
+  before(:each) {
+    @user = User.new("tatiana.soukiassian@gmail.com", "carrots")
+  }
+
   it 'should be initialized with an email and password' do
-    user = User.new("tatiana.soukiassian@gmail.com", "carrots")
-    expect(user.email).to eq "tatiana.soukiassian@gmail.com"
-    expect(user.password).to eq "carrots"
+    expect(@user.email).to eq "tatiana.soukiassian@gmail.com"
+    expect(@user.password).to eq "carrots"
+  end
+
+  it 'should allow to login' do
+  	@user.login
+  	session_id = @user.my_cookie["my_session_id"]
+  	expect(session_id.length).to be >= 32
   end
 
 end
