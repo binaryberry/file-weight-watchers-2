c632cee308e1224338f823f835760d6281896ced
Tatiana Soukiassian <binaryberry@gmail.com>
2015-02-11 00:16:00 +0000

    knows the number of files for text files

--- app/lib/file_list.rb
+++ app/lib/file_list.rb
@@ -1,11 +1,16 @@
 require 'pry'
 class FileList
 
-	attr_accessor :files_metadata, :files_weight
+	attr_accessor :files_metadata, :files_weight, :text_files
 
 	def initialize(metadata)
 		@files_metadata = metadata
 		@files_weight = 0
+		@text_files = {}
+		@categories = [@text_files]
+		@extensions = {@text_files => ["jpg"]}
+		@gravities = {@text_files => 1}
+		get_category_data
 	end
 
 	def total_weight
@@ -14,4 +19,29 @@ class FileList
 		end
 		@files_weight
 	end
+
+	def get_category_data
+		@categories.each do |category|
+			get_file_number(category)
+			# get_category_weight(category)
+		end
+	end
+
+	def get_file_number(category)
+		number = 0
+		@files_metadata["files"].each do |file_metadata|
+			number +=1 if @extensions[category].include?(file_metadata["extension"]) 
+		end
+		category["file_number"] = number
+	end
+
+	# def get_category_weight(category)
+	# 	weight = 0
+	# 	@files_metadata["files"].each do |file_metadata|
+	# 		weight += file_metadata["size"] * @gravities[category] if @extensions[category].include?(file_metadata["extension"]) 
+	# 		weight += 100 if category = @text_files
+	# 	end
+	# 	category["weight"] = weight
+	# end
+
 end
--- spec/file_list_spec.rb
+++ spec/file_list_spec.rb
@@ -5,7 +5,7 @@ require 'pry'
 describe 'file_list' do
 
   before(:each) {
-  	@metadata = {"pagination"=>{"current_page"=>1, "total_pages"=>1, "total_entries"=>3},"files"=>[{"id"=>14307204,"name"=>"frozen","extension"=>"jpg","version"=>1,"size"=>44235,"created_at"=>"2015-01-18T18:38:30Z","updated_at"=>"2015-01-18T18:39:09Z","creator"=>{"name"=>"Tatiana Soukiassian", "email"=>"tatiana.soukiassian@gmail.com", "uuid"=>"f88a294c-8e2d-436c-a16d-70ce35df4aa3"},"url"=>"https://my.workshare.com/decks/14307204","access_token"=>"2AM-A6UTzskGR5Gd","first_page_url"=>"https://my.workshare.com/images/icons/medium/jpg.png","folder_id"=>1117472},{"id"=>14307202,"name"=>"README","extension"=>"md","version"=>1,"size"=>11093,"created_at"=>"2015-01-18T18:38:12Z","updated_at"=>"2015-01-18T18:38:15Z","creator"=>{"name"=>"Tatiana Soukiassian", "email"=>"tatiana.soukiassian@gmail.com", "uuid"=>"f88a294c-8e2d-436c-a16d-70ce35df4aa3"},"url"=>"https://my.workshare.com/decks/14307202","access_token"=>"AlGalHjo-9hw-HV3","first_page_url"=>"https://my.workshare.com/images/icons/medium/unknown.png","folder_id"=>1117472},{"id"=>14307198,"name"=>"cat","extension"=>"jpeg","version"=>1,"size"=>9762,"created_at"=>"2015-01-18T18:38:05Z","updated_at"=>"2015-01-18T18:38:28Z","creator"=>{"name"=>"Tatiana Soukiassian", "email"=>"tatiana.soukiassian@gmail.com", "uuid"=>"f88a294c-8e2d-436c-a16d-70ce35df4aa3"},"url"=>"https://my.workshare.com/decks/14307198","access_token"=>"XRZXnK6ZNZBWl64v","first_page_url"=>"https://my.workshare.com/images/icons/medium/jpg.png","folder_id"=>1117472}]}
+  	@metadata = {"pagination"=>{"current_page"=>1, "total_pages"=>1, "total_entries"=>4}, "files"=>[{"id"=>14726726, "name"=>"Copie de Reporting Janvier SNI - TS", "extension"=>"xlsx", "version"=>1, "size"=>745563, "created_at"=>"2015-02-10T23:52:15Z", "updated_at"=>"2015-02-10T23:53:38Z", "creator"=>{"name"=>"Tatiana Soukiassian", "email"=>"tatiana.soukiassian@gmail.com", "uuid"=>"f88a294c-8e2d-436c-a16d-70ce35df4aa3"}, "url"=>"https://my.workshare.com/decks/14726726", "access_token"=>"OnhRcxVV6Maw-jHl", "first_page_url"=>"https://my.workshare.com/images/icons/medium/xls.png", "folder_id"=>1117472}, {"id"=>14307204, "name"=>"frozen", "extension"=>"jpg", "version"=>1, "size"=>44235, "created_at"=>"2015-01-18T18:38:30Z", "updated_at"=>"2015-01-18T18:39:09Z", "creator"=>{"name"=>"Tatiana Soukiassian", "email"=>"tatiana.soukiassian@gmail.com", "uuid"=>"f88a294c-8e2d-436c-a16d-70ce35df4aa3"}, "url"=>"https://my.workshare.com/decks/14307204", "access_token"=>"2AM-A6UTzskGR5Gd", "first_page_url"=>"https://my.workshare.com/images/icons/medium/jpg.png", "folder_id"=>1117472}, {"id"=>14307202, "name"=>"README", "extension"=>"txt", "version"=>2, "size"=>11093, "created_at"=>"2015-01-18T18:38:12Z", "updated_at"=>"2015-02-10T23:52:18Z", "creator"=>{"name"=>"Tatiana Soukiassian", "email"=>"tatiana.soukiassian@gmail.com", "uuid"=>"f88a294c-8e2d-436c-a16d-70ce35df4aa3"}, "url"=>"https://my.workshare.com/decks/14307202", "access_token"=>"AlGalHjo-9hw-HV3", "first_page_url"=>"https://my.workshare.com/images/icons/medium/txt.png", "folder_id"=>1117472}, {"id"=>14307198, "name"=>"cat", "extension"=>"jpeg", "version"=>1, "size"=>9762, "created_at"=>"2015-01-18T18:38:05Z", "updated_at"=>"2015-01-18T18:38:28Z", "creator"=>{"name"=>"Tatiana Soukiassian", "email"=>"tatiana.soukiassian@gmail.com", "uuid"=>"f88a294c-8e2d-436c-a16d-70ce35df4aa3"}, "url"=>"https://my.workshare.com/decks/14307198", "access_token"=>"XRZXnK6ZNZBWl64v", "first_page_url"=>"https://my.workshare.com/images/icons/medium/jpg.png", "folder_id"=>1117472}]}
     @file_list = FileList.new(@metadata)
   }
 
@@ -14,7 +14,15 @@ describe 'file_list' do
   end
 
   it 'should know the total weight of the files' do
-    expect(@file_list.total_weight).to eq 65090
+    expect(@file_list.total_weight).to eq 810653 #this is the total file weight of test user
   end
+
+  it 'should know the number of files for text files' do
+    expect(@file_list.text_files["file_number"]).to eq 1
+  end
+
+  # it 'should know the weight of files for text files' do
+  #   expect(@file_list.text_files["weight"]).to eq 1
+  # end
 
 end
