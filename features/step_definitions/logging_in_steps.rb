22517db2fe22ad3bb833ccf95b90cad3e81acbec
Tatiana Soukiassian <binaryberry@gmail.com>
2015-02-16 09:57:38 +0000

    removed web steps from features folder

--- features/step_definitions/logging_in_steps.rb
+++ features/step_definitions/logging_in_steps.rb
@@ -7,3 +7,16 @@ end
 Then(/^I should see the weight of my files$/) do
   expect(page).to have_content "total weight"
 end
+
+Given(/^I am on the homepage$/) do
+  visit '/'
+end
+
+Then(/^I should be on the file_list page$/) do
+  visit '/file_list'
+end
+
+Then(/^I should see "(.*?)"$/) do |arg1|
+	expect(page).to have_content arg1
+end
+
