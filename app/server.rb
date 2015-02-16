3c0dd17ffbe860e3672fdd5019b10efd56a5b384
Tatiana Soukiassian <binaryberry@gmail.com>
2015-02-16 01:12:40 +0000

    updated public folder path in server

--- app/server.rb
+++ app/server.rb
@@ -11,8 +11,8 @@ class Fileww < Sinatra::Application
 
 	enable :sessions
 	set :session_secret, 'super secret'
-	set :public_folder, '../public'
-		
+	set :public_folder, File.join(File.dirname(__FILE__), '..', 'public')		
+	
 	get '/' do
         erb :index
 	end
